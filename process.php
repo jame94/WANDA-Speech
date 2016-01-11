c3386e12891e661f24714a8bbf1390df5351c9a6
Jan Messing <jan.messing@arcor.de>
2016-01-11 19:31:19 +0100

    atabase-problem

--- process.php
+++ process.php
@@ -1,49 +1,67 @@
 <?php 
-error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include('database.php');
+
+$DATABASE = new Database("db12483686-ad2", "PASSWORT", "dbu12483686-wanda", "localhost"); //WP282.WEBPACK.HOSTEUROPE.DE
+
+$anfrage = $_GET['anfrage'];
+
+$filter = array( $anfrage ); 
+print_r( $filter );
+$antwort = $DATABASE->select("SELECT `action` FROM `interactiveSpeech` WHERE `attention` = ?; ", $filter, array("%s")); 
+print_r($antwort);
+
+if(count($antwort) != 0)
+      echo $antwort[0]->action;
 
 
-class Database
-{
-  private $connection = NULL;
-  private $result = NULL;
-  
-  
-  public function __construct($host = "localhost", $user = "dbu12483686-admin", $pass = "rn3xfyfjJV4Mgq3", $db = "dbu12483686-maindata")
-  {
-    $connection = mysql_connect($host,$user,$pass) or die("### Connection could not be established! ###"); 
-    mysql_select_db($db) or die("### Database could not be selected! ###".mysql_error());
-  }
-  
-  public function exe($statement)
-  {
-    return $this->result = mysql_query($statement) or die("### Error in SQL-Syntax ###<br>".mysql_error());
-  }
-  
-  public function get()
-  {
-    return mysql_fetch_assoc($this->result);
-  }
-  
-  public function num()
-  {
-    return mysql_num_rows($this->result);
-  }
-  
-  
-}
 
-$DB = new Database();
+//
+//
+// error_reporting(E_ALL);
+//
+//
+//class Database
+//{
+//  private $connection = NULL;
+//  private $result = NULL;
+//
+//
+//  public function __construct($host = "localhost", $user = "dbu12483686-admin", $pass = "rn3xfyfjJV4Mgq3", $db = "dbu12483686-maindata")
+//  {
+//    $connection = mysql_connect($host,$user,$pass) or die("### Connection could not be established! ###");
+//    mysql_select_db($db) or die("### Database could not be selected! ###".mysql_error());
+//  }
+//
+//  public function exe($statement)
+//  {
+//    return $this->result = mysql_query($statement) or die("### Error in SQL-Syntax ###<br>".mysql_error());
+//  }
+//
+//  public function get()
+//  {
+//    return mysql_fetch_assoc($this->result);
+//  }
+//
+//  public function num()
+//  {
+//    return mysql_num_rows($this->result);
+//  }
+//
+//
+//}
+//
+//$DB = new Database();
+//
+//$anfrage = "SELECT * FROM `interactiveSpeech` WHERE attention = '".mysql_real_escape_string($_GET['anfrage'])."'; ";
+//
+//
+//$eintraege = $DB->exe($anfrage);
+//$ergebnis = $DB->get($eintraege);
+//
+//echo $ergebnis['action'];
+//
 
-$anfrage = "SELECT * FROM 'interactiveSpeech' WHERE attention = '"mysql_real_escape_string($_GET['anfrage'])"'; ";
-
-/*
-$query = sprintf("SELECT * FROM users WHERE user='%s' AND password='%s'", mysql_real_escape_string($user), mysql_real_escape_string($password));*/
-
-$eintraege = $DB->exe($anfrage);
-$ergebnis = $DB->get($eintraege);
-
-echo $ergebnis['action');
-
-//echo "sayHelloWanda";
 
 ?>
