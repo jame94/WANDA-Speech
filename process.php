22a8ca2c8cb438b88c24c91fb31839038dfbc602
Jan Messing <jan.messing@arcor.de>
2016-01-14 10:35:38 +0100

    sayHelloWanda

--- process.php
+++ process.php
@@ -1,60 +1,23 @@
 <?php 
-ini_set('display_errors', 1);
 
-include('database.php');
+// 1. Create a database connection
+$connection = mysqli_connect("localhost","db12483686-admin","mNQgb44yt7h7tT27");
+    if (!$connection) {
+        die("Database connection failed: " . mysqli_error());
+    } 
+    
+// 2. Select a database to use 
+$db_select = mysqli_select_db($connection, "db12483686-maindata");
+    if (!$db_select) {
+        die("Database selection failed: " . mysqli_error());
+    } 
 
-$DATABASE = new Database("dbu12483686-admin", "PASS", "dbu12483686-maindata", "localhost"); //WP282.WEBPACK.HOSTEUROPE.DE
-
-$anfrage = $_GET['anfrage'];
-
-$filter = array( $anfrage ); 
-print_r( $filter );
-$antwort = $DATABASE->select("SELECT `action` FROM `interactiveSpeech` WHERE `attention` = ?; ", $filter, array("%s")); 
-print_r($antwort);
-
-if(count($antwort) != 0)
-      echo $antwort[0]->action;
-
-class Database
-{
-  private $connection = NULL;
-  private $result = NULL;
+$anfrage = mysqli_real_escape_string($connection, $_GET['anfrage']);
+$ergebnis = mysqli_query($connection,"SELECT * FROM `interactiveSpeech` WHERE `attention` = '".$anfrage."'") or die("Error in SQL-Syntax<br>".mysqli_error());
+$result = mysqli_fetch_assoc($ergebnis);
+echo $result['action'];
 
 
-  public function __construct($host = "localhost", $user = "dbu12483686-admin", $pass = "PASS", $db = "dbu12483686-maindata")
-  {
-    $connection = mysql_connect($host,$user,$pass) or die("### Connection could not be established! ###");
-    mysql_select_db($db) or die("### Database could not be selected! ###".mysql_error());
-  }
-
-  public function exe($statement)
-  {
-    return $this->result = mysql_query($statement) or die("### Error in SQL-Syntax ###<br>".mysql_error());
-  }
-
-  public function get()
-  {
-    return mysql_fetch_assoc($this->result);
-  }
-
-  public function num()
-  {
-    return mysql_num_rows($this->result);
-  }
-
-
-}
-
-$DB = new Database();
-
-$anfrage = "SELECT * FROM `interactiveSpeech` WHERE attention = '".mysql_real_escape_string($_GET['anfrage'])."'; ";
-
-
-$eintraege = $DB->exe($anfrage);
-$ergebnis = $DB->get($eintraege);
-
-echo $ergebnis['action'];
-
-
+mysqli_close($connection);
 
 ?>
